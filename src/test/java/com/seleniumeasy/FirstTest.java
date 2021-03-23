f976756b08d531f698abe0149419408191de7896
AleksTrig <Alekstrig@gmail.com>
2021-03-23 18:49:52 +0200

    The start of automation

--- src/test/java/com/seleniumeasy/FirstTest.java
+++ src/test/java/com/seleniumeasy/FirstTest.java
@@ -1,9 +1,10 @@
 package com.seleniumeasy;
 
-import org.junit.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
 
 public class FirstTest {
     @Test
