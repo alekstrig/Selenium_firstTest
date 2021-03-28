595c67d9b92c65814c0c01b441ca041d9044bfb6
AleksTrig <Alekstrig@gmail.com>
2021-03-28 11:49:51 +0300

    Is done Third test Forth in process, last step

--- src/test/java/com/seleniumeasy/BaseTest.java
+++ src/test/java/com/seleniumeasy/BaseTest.java
@@ -2,6 +2,7 @@ package com.seleniumeasy;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -89,8 +90,7 @@ public class BaseTest {   //extends
         WebElement checkElementTotal = driver.findElement(By.xpath("//span[@id = 'displayvalue']"));
         checkElementTotal.getText();
         Assert.assertEquals(checkElementTotal.getText(),"15");
-
-        //driver.quit();
+        driver.quit();
 
 
     }
@@ -121,17 +121,53 @@ public class BaseTest {   //extends
         WebElement multipleCheckDemoTwo = driver.findElement(By.xpath("//label[text()='Option 3']"));
         multipleCheckDemoTwo.click();
 
-
-
-
         Assert.assertTrue(multipleCheckDemoOne.isEnabled(),"Falls");
         Assert.assertTrue(multipleCheckDemoTwo.isEnabled(),"Falls");
 
-
-        //driver.quit();
+        driver.quit();
 
 
     }
+    @Test
+    public void forthTest() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.seleniumeasy.com/test/"); //visibilityOfAllElementsLocatedBy
+        try{
+            new WebDriverWait(driver,5).until(ExpectedConditions
+                    .visibilityOfElementLocated(By.xpath("//*[@id = 'at-cv-lightbox-close']")));
+            WebElement destrButton = driver.findElement(By.xpath("//*[@id = 'at-cv-lightbox-close']"));
+            destrButton.click();
+        }
+        catch (Exception e){
+            System.out.println("hjdsgfjh");
+        }
+        WebElement inputForms = driver.findElement(By.xpath("//a[text()='Input Forms']"));
+        inputForms.click();
+
+        WebElement radioButtonsDemo = driver.findElement(By.xpath("//li[@class='tree-branch']/ul/li[3]/a[text() = 'Radio Buttons Demo']"));
+        radioButtonsDemo.click();
+
+        WebElement sexMale = driver.findElement(By.xpath("//h4[text() = 'Sex : ']/following-sibling::label[1]"));
+        JavascriptExecutor executorMale = (JavascriptExecutor)driver;
+        executorMale.executeScript("arguments[0].click();", sexMale);
+
+        WebElement ageGroup = driver.findElement(By.xpath("//input[@value = '5 - 15']"));
+        JavascriptExecutor executorAge = (JavascriptExecutor)driver;
+        executorAge.executeScript("arguments[0].click();", ageGroup);
+
+        WebElement getValues = driver.findElement(By.xpath("//div[@class ='panel-body']/button[@class = 'btn btn-default']"));
+        getValues.click();
+
+
+
+
+    }
+
+
+
+
+
 
 
 }
