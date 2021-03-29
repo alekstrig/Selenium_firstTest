407c29dad094e6d22479ff5f4c7b68f6660d8e33
AleksTrig <Alekstrig@gmail.com>
2021-03-29 19:53:02 +0300

    is done Eighth test Ninth test in process

--- src/test/java/com/seleniumeasy/EighthTest.java
+++ src/test/java/com/seleniumeasy/EighthTest.java
@@ -6,8 +6,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.LocalDateTime;
@@ -34,17 +37,25 @@ public class EighthTest {
 
         WebElement setDate = driver.findElement(By.xpath("//input[@placeholder = 'dd/mm/yyyy']"));
         setDate.click();
+        Actions actions = new Actions(driver);
+        actions.moveToElement(setDate).click(setDate).build().perform();
+
         //setDate.sendKeys(Keys.ENTER);
         LocalDateTime today = LocalDateTime.now();
-        WebElement setDay = driver.findElement(By.xpath("//div[@class = 'datepicker-days']/table/tfoot/tr/th[text() = 'Clear']"));
-        setDay.click();
+        //WebElement setDay = driver.findElement(By.xpath("//div[@class = 'datepicker-days']/table/tfoot/tr/th[text() = 'Clear']"));
+        //setDay.click();
 
         LocalDateTime yesterday = today.minusDays(1); //today: "2021-03-26T20:11:
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String correctDate = yesterday.format(formatter);
 
         setDate.sendKeys(correctDate);
-        System.out.println(correctDate);
+
+
+        WebElement checkSetDate = driver.findElement(By.xpath("//input[@placeholder = 'dd/mm/yyyy']"));
+        Assert.assertEquals(checkSetDate.getAttribute("value"), "28/03/2021");
+
+        driver.quit();
 
 
 
