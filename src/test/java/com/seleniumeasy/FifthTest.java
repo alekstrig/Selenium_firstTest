a4d510d6de4cbc7fed34a1d5469bb7b3e489aae9
AleksTrig <Alekstrig@gmail.com>
2021-04-03 13:37:04 +0300

    is done Eleventh test Tenth test in process add BaseTest in process Jenkins v.05

--- src/test/java/com/seleniumeasy/FifthTest.java
+++ src/test/java/com/seleniumeasy/FifthTest.java
@@ -11,9 +11,22 @@ import org.testng.internal.BaseTestMethod;
 
 import java.security.Key;
 
-public class FifthTest  {  //extends PrimeTest
+public class FifthTest extends BaseTest  {  //extends PrimeTest
+
+    By dropDownList = By.xpath("//li[@class = 'tree-branch']/*/li/a[text() = 'Select Dropdown List']");
+    By selectDemo = By.xpath("//select[@id = 'select-demo']");
+    By selectDay = By.xpath("//select[@id='select-demo']/option[text() = 'Tuesday']");
+
+
     @Test
     public void fifthTest() {
+
+     find(inputFormMenuItem).click();
+     find(dropDownList).click();
+     find(selectDemo).sendKeys(Keys.ENTER);
+     find(selectDay).click();
+
+    /*public void fifthTest() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.seleniumeasy.com/test/"); //visibilityOfAllElementsLocatedBy
@@ -24,24 +37,20 @@ public class FifthTest  {  //extends PrimeTest
             destrButton.click();
         } catch (Exception e) {
             System.out.println("hjdsgfjh");
-        }
+        }*/
 
-        WebElement inputForms = driver.findElement(By.xpath("//a[text()='Input Forms']"));
-        inputForms.click();
+        //WebElement inputForms = driver.findElement(By.xpath("//a[text()='Input Forms']"));
+        //inputForms.click();
 
-        WebElement dropDownList = driver.findElement(By.xpath("//li[@class = 'tree-branch']/*/li/a[text() = 'Select Dropdown List']"));
-        dropDownList.click();
+        //WebElement dropDownList = driver.findElement(By.xpath("//li[@class = 'tree-branch']/*/li/a[text() = 'Select Dropdown List']"));
+        //dropDownList.click();
 
-        WebElement selectDemo = driver.findElement(By.xpath("//select[@id = 'select-demo']"));
-        selectDemo.sendKeys(Keys.ENTER);
-        WebElement selectDay = driver.findElement(By.xpath("//select[@id='select-demo']/option[text() = 'Tuesday']"));
-        selectDay.click();
-
-        Assert.assertEquals(selectDay.getText(), "Tuesday");
-
-
-
-
+        //WebElement selectDemo = driver.findElement(By.xpath("//select[@id = 'select-demo']"));
+        //selectDemo.sendKeys(Keys.ENTER);
+        //WebElement selectDay = driver.findElement(By.xpath("//select[@id='select-demo']/option[text() = 'Tuesday']"));
+        //selectDay.click();
+        String text = find(selectDay).getText();
+        Assert.assertEquals(text, "Tuesday");
 
     }
 }
